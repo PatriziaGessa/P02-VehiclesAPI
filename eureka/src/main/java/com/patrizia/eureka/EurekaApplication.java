27ce893799fa15d6c1ae3b4bc4c4498be46b0797
PatriziaGessa <patriziagessa@gmail.com>
2021-12-04 00:23:58 +0100

    fixed error

--- eureka/src/main/java/com/patrizia/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/patrizia/eureka/EurekaApplication.java
@@ -2,14 +2,14 @@ package com.patrizia.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaServer
 public class EurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
 
 }
